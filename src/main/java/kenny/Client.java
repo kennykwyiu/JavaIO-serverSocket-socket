1552f81cba05f70e11f723566e42a11bdff4efb7
kenny <kenny.kwyiu@gmail.com>
2024-04-05 20:33:22 +0800

    5 Apr 2024 - Client

---
+++ src/main/java/kenny/Client.java
@@ -0,0 +1,11 @@
+package kenny;
+
+import java.net.Socket;
+
+public class Client {
+    public static void main(String[] args) {
+        final String DEFAULT_SERVER_HOST = "127.0.0.1";
+        final int DEFAULT_SERVER_PORT = 888;
+        Socket socket = null;
+    }
+}
